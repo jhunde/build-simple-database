fafc753cad9b0e0dc33e804ae6dc516bf6720427
jhunde <jena.kelbessa@gmail.com>
2025-10-16 23:51:31 -0700

    feat: added fuctions read_line and close_input_buffer

--- main.c
+++ main.c
@@ -19,13 +19,35 @@ void print_prompt(){
     printf("db > ");
 }
 
+// Read a line of input using getline()
+ssize_t getline(char** lineptr, size_t* n, FILE* stream);
+
+void read_input(InputBuffer* input_buffer){
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_lenght), stdin);
+
+    if (bytes_read <= 0){
+        printf("Error read input \n");
+        exit(EXIT_SUCCESS);
+    }
+
+    // Ignore trailing newline
+    input_buffer->input_length = bytes_read-1;
+    input_buffer->buffer[buffer_read-1] = 0 
+}
+
+// Free the allocated memory
+void close_input_buffer(InputBuffer* input_buffer){
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
 int main(int argc, char* argc[]){
 
     // Get a line of input, then process that line of input
     InputBuffer* input_buffer = new_input_buffer();
     while (true){
         print_prompt();
-        read_line(input_buffer);
+        read_input(input_buffer);
 
         // valid input? 
         if (strcmp(input_buffer->buffer, ".exit") == 0){
