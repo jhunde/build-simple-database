5ebd7153119e1aa6a57526e8874ec301437961c5
hunde <jena.kelbessa@gmail.com>
2025-10-20 23:53:13 -0700

    fixed bugs in main.c for code to work. It works!

--- main.c
+++ main.c
@@ -1,62 +1,79 @@
-// Define InputBuffer
+#define MAX_INPUT_SIZE 1024
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct {
-    char* buffer;
-    size_t buffer_lenght;
-    ssize_t input_buffer;
+    char*  buffer;
+    size_t buffer_length;   // capacity
+    size_t input_length;    // number of chars (excl. '\0')
 } InputBuffer;
 
-InputBuffer* new_input_buffer(){
-    InputBuffer* input_buffer = (InputBuffer*)molloc(sizeof(InputBuffer));
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_lenght = 0;
-    input_buffer->input_buffer = 0;
-
+InputBuffer* new_input_buffer(void) {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    if (!input_buffer) {
+        fprintf(stderr, "malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    input_buffer->buffer = (char*)malloc(MAX_INPUT_SIZE);
+    if (!input_buffer->buffer) {
+        fprintf(stderr, "malloc failed\n");
+        free(input_buffer);
+        exit(EXIT_FAILURE);
+    }
+    input_buffer->buffer_length = MAX_INPUT_SIZE;
+    input_buffer->input_length  = 0;
     return input_buffer;
 }
 
-// This fuction will be called before reading each line of the input
-void print_prompt(){
+void print_prompt(void) {
     printf("db > ");
+    fflush(stdout); // make sure prompt shows up
 }
 
-// Read a line of input using getline()
-ssize_t getline(char** lineptr, size_t* n, FILE* stream);
-
-void read_input(InputBuffer* input_buffer){
-    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_lenght), stdin);
-
-    if (bytes_read <= 0){
-        printf("Error read input \n");
-        exit(EXIT_SUCCESS);
+void read_input(InputBuffer* input_buffer) {
+    if (!fgets(input_buffer->buffer, (int)input_buffer->buffer_length, stdin)) {
+        if (feof(stdin)) {
+            // Treat Ctrl+Z/EOF as clean exit
+            printf("\n");
+            free(input_buffer->buffer);
+            free(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("fgets");
+            free(input_buffer->buffer);
+            free(input_buffer);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // Ignore trailing newline
-    input_buffer->input_length = bytes_read-1;
-    input_buffer->buffer[buffer_read-1] = 0 
+    // strip trailing newline if present
+    size_t len = strlen(input_buffer->buffer);
+    if (len > 0 && input_buffer->buffer[len - 1] == '\n') {
+        input_buffer->buffer[len - 1] = '\0';
+        len--;
+    }
+    input_buffer->input_length = len;
 }
 
-// Free the allocated memory
-void close_input_buffer(InputBuffer* input_buffer){
+void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
 }
 
-int main(int argc, char* argc[]){
-
-    // Get a line of input, then process that line of input
+int main(void) {
     InputBuffer* input_buffer = new_input_buffer();
-    while (true){
+    while (true) {
         print_prompt();
         read_input(input_buffer);
 
-        // valid input? 
-        if (strcmp(input_buffer->buffer, ".exit") == 0){
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
             close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
-        }
-        // invalid input?
-        else {
-            printf("Unrecognized command '%s' .\n", input_buffer->buffer);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
         }
     }
 }
